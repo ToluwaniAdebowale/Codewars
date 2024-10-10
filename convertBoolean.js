75007315bb62bd516e1f4d8a9f6a0734602700cc
ToluwaniAdebowale <glonimi0@gmail.com>
2024-10-10 08:39:48 +0100

    Create convertBoolean.js

---
+++ convertBoolean.js
@@ -0,0 +1,13 @@
+// https://codewars.com/kata/53369039d7ab3ac506000467
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// return "Yes" if the boolean is true
+// return "No" if the boolean is false
+
+function boolToWord( bool ){
+  if (bool === true){
+    return "Yes";
+  }
+  return "No";
+}
